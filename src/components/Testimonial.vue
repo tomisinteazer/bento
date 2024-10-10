61d55c73fede80dfb38d3d2060bdddb5413c817b
Tomisinteazer <tomisinteazer@gmail.com>
2024-10-10 05:03:39 +0100

    designed a bit , refactored a lot

---
+++ src/components/Testimonial.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <section class="text-zinc-400 invert-bg body-font">
+            <div class="container border border-t-0 border-b-0 border-zinc-600 px-5 py-24 mx-auto">
+                <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                        class="inline-block w-8 h-8 text-zinc-500 mb-8" viewBox="0 0 975.036 975.036">
+                        <path
+                            d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z">
+                        </path>
+                    </svg>
+                    <p class="leading-relaxed text-lg">Our deepest fear is not that we are inadequate. Our deepest fear
+                        is that we are powerful beyond measure. It is our light, not our darkness that most frightens
+                        us. We ask ourselves, 'Who am I to be brilliant, gorgeous, talented, fabulous?' Actually, who
+                        are you not to be? You are a child of God. Your playing small does not serve the world. There is
+                        nothing enlightened about shrinking so that other people won't feel insecure around you. We are
+                        all meant to shine, as children do. We were born to make manifest the glory of God that is
+                        within us. It's not just in some of us; it's in everyone. And as we let our own light shine, we
+                        unconsciously give other people permission to do the same. As we are liberated from our own
+                        fear, our presence automatically liberates others.</p>
+                    <span class="inline-block h-1 w-10 rounded bg-[#85c227] mt-8 mb-6"></span>
+                    <h2 class="text-white font-medium title-font tracking-wider text-sm text-uppercase">Marianne
+                        Williamson</h2>
+                    <p class="text-zinc-500"> A Return to Love: Reflections on the Principles of "A Course in Miracles"
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Projects from '@/components/Projects.vue';
+import Testimonial from '@/components/Testimonial.vue';
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Projects />
+    <Testimonial />
+
+
   </main>
+
+
+
 </template>
+<style>
+img {
+  opacity: 20%;
+}
+
+img:hover {
+  opacity: 100%;
+}
+</style>

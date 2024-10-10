647d660beaebe2318b5329d2e6dd9f48bcd1929a
Tomisinteazer <tomisinteazer@gmail.com>
2024-10-10 05:04:15 +0100

    small touches to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,13 +3,13 @@
 
     <div class=" mx-auto container  border-l border-r  px-5 py-10 ">
       <div class=" flex flex-wrap  justify-end ">
-        <h2 class="tracking-widest text-xs title-font font-medium text-gray-300 mb-1">Works of Art</h2>
+        <h2 class="tracking-widest text-xs title-font font-medium text-zinc-300 mb-1">Works of Art</h2>
       </div>
       <div class=" flex flex-wrap  ">
         <div class="">
 
           <h1 class="title-font text-2xl font-bold text-black mb-3">Projects</h1>
-          <p class="leading-relaxed mb-3 text-gray-500 max-w-2xl break-words">Bringing big ideas to life with projects
+          <p class="leading-relaxed mb-3 text-zinc-500 max-w-2xl break-words">Bringing big ideas to life with projects
             that mix
             clever tech!
             Building cool solutions that make businesses smarter and the future brighter—one
@@ -26,20 +26,20 @@
           </div>
         </div>
 
-        <section class="text-gray-600 body-font">
+        <section class="text-zinc-600 body-font">
           <div class="container  py-10 ">
             <div class="flex flex-wrap ">
               <div class="py-4  md:w-1/2 md:pr-4">
-                <div class="h-full border border-gray-200 border-opacity-60  overflow-hidden">
+                <div class="h-full border border-zinc-200 border-opacity-60  overflow-hidden">
                   <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/1280x400"
                     alt="blog">
                   <div class="p-6">
-                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+                    <h2 class="tracking-widest text-xs title-font font-medium text-zinc-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-zinc-900 mb-3">The Catalyzer</h1>
                     <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                       microdosing tousled waistcoat.</p>
                     <div class="flex items-center flex-wrap ">
-                      <a class="text-gray-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <a class="text-zinc-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round">
                           <path d="M5 12h14"></path>
@@ -47,14 +47,14 @@
                         </svg>
                       </a>
                       <span
-                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        class="text-zinc-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-200">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>1.2K
                       </span>
-                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <span class="text-zinc-400 inline-flex items-center leading-none text-sm">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path
@@ -67,16 +67,16 @@
                 </div>
               </div>
               <div class="py-4 md:w-1/4 md:pr-4">
-                <div class="h-full border border-gray-200 border-opacity-60  overflow-hidden">
+                <div class="h-full border border-zinc-200 border-opacity-60  overflow-hidden">
                   <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401"
                     alt="blog">
                   <div class="p-6">
-                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                    <h2 class="tracking-widest text-xs title-font font-medium text-zinc-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-zinc-900 mb-3">The 400 Blows</h1>
                     <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                       microdosing tousled waistcoat.</p>
                     <div class="flex items-center flex-wrap">
-                      <a class="text-gray-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <a class="text-zinc-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round">
                           <path d="M5 12h14"></path>
@@ -84,14 +84,14 @@
                         </svg>
                       </a>
                       <span
-                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        class="text-zinc-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-200">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>1.2K
                       </span>
-                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <span class="text-zinc-400 inline-flex items-center leading-none text-sm">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path
@@ -104,16 +104,16 @@
                 </div>
               </div>
               <div class="py-4 md:w-1/4 md:pr-4">
-                <div class="h-full border border-gray-200 border-opacity-60  overflow-hidden">
+                <div class="h-full border border-zinc-200 border-opacity-60  overflow-hidden">
                   <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402"
                     alt="blog">
                   <div class="p-6">
-                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                    <h2 class="tracking-widest text-xs title-font font-medium text-zinc-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-zinc-900 mb-3">Shooting Stars</h1>
                     <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                       microdosing tousled waistcoat.</p>
                     <div class="flex items-center flex-wrap ">
-                      <a class="text-gray-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <a class="text-zinc-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round">
                           <path d="M5 12h14"></path>
@@ -121,14 +121,14 @@
                         </svg>
                       </a>
                       <span
-                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        class="text-zinc-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-200">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>1.2K
                       </span>
-                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <span class="text-zinc-400 inline-flex items-center leading-none text-sm">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path
@@ -141,16 +141,16 @@
                 </div>
               </div>
               <div class="py-4  md:w-1/2 md:pr-4">
-                <div class="h-full border border-gray-200 border-opacity-60  overflow-hidden">
+                <div class="h-full border border-zinc-200 border-opacity-60  overflow-hidden">
                   <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/1280x400"
                     alt="blog">
                   <div class="p-6">
-                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+                    <h2 class="tracking-widest text-xs title-font font-medium text-zinc-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-zinc-900 mb-3">The Catalyzer</h1>
                     <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                       microdosing tousled waistcoat.</p>
                     <div class="flex items-center flex-wrap ">
-                      <a class="text-gray-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <a class="text-zinc-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round">
                           <path d="M5 12h14"></path>
@@ -158,14 +158,14 @@
                         </svg>
                       </a>
                       <span
-                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        class="text-zinc-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-200">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>1.2K
                       </span>
-                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <span class="text-zinc-400 inline-flex items-center leading-none text-sm">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path
@@ -178,16 +178,16 @@
                 </div>
               </div>
               <div class="py-4 md:w-1/4 md:pr-4">
-                <div class="h-full border border-gray-200 border-opacity-60  overflow-hidden">
+                <div class="h-full border border-zinc-200 border-opacity-60  overflow-hidden">
                   <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401"
                     alt="blog">
                   <div class="p-6">
-                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                    <h2 class="tracking-widest text-xs title-font font-medium text-zinc-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-zinc-900 mb-3">The 400 Blows</h1>
                     <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                       microdosing tousled waistcoat.</p>
                     <div class="flex items-center flex-wrap">
-                      <a class="text-gray-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <a class="text-zinc-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round">
                           <path d="M5 12h14"></path>
@@ -195,14 +195,14 @@
                         </svg>
                       </a>
                       <span
-                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        class="text-zinc-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-200">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>1.2K
                       </span>
-                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <span class="text-zinc-400 inline-flex items-center leading-none text-sm">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path
@@ -215,16 +215,16 @@
                 </div>
               </div>
               <div class="py-4 md:w-1/4 md:pr-4">
-                <div class="h-full border border-gray-200 border-opacity-60  overflow-hidden">
+                <div class="h-full border border-zinc-200 border-opacity-60  overflow-hidden">
                   <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402"
                     alt="blog">
                   <div class="p-6">
-                    <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                    <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                    <h2 class="tracking-widest text-xs title-font font-medium text-zinc-400 mb-1">CATEGORY</h2>
+                    <h1 class="title-font text-lg font-medium text-zinc-900 mb-3">Shooting Stars</h1>
                     <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                       microdosing tousled waistcoat.</p>
                     <div class="flex items-center flex-wrap ">
-                      <a class="text-gray-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                      <a class="text-zinc-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round">
                           <path d="M5 12h14"></path>
@@ -232,14 +232,14 @@
                         </svg>
                       </a>
                       <span
-                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        class="text-zinc-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-zinc-200">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>1.2K
                       </span>
-                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <span class="text-zinc-400 inline-flex items-center leading-none text-sm">
                         <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none"
                           stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                           <path
